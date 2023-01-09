7df16ddeb1f9a602511a8050780b26b91131fad1
Raymond Lee <rlee2@instructors.2u.com>
2023-01-09 09:24:42 -0800

    copies landscape photos to Landscape folder

--- spotlight_python.py
+++ spotlight_python.py
@@ -1,8 +1,7 @@
 import os
-#from PIL import Image
-#from PIL.ExifTags import TAGS
 import shutil
 from datetime import date
+import cv2
 
 #change C:\\User\\newrl to C:\\Users\\$your_directory
 home_directory = "newrl"
@@ -22,8 +21,7 @@ while i < length:
 print(photos)
 
 #copy photos to new folder and renames photo with jpg
-i = 0
-length = len(photos)
+
 today = date.today().strftime("%m-%d-%Y")
 foldername = f"Spotlight_Pictures_{today}"
 if not os.path.exists(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight"):
@@ -31,26 +29,25 @@ if not os.path.exists(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotligh
 if not os.path.exists(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\{foldername}"):    
     os.mkdir(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\{foldername}")
 print(foldername)
+i = 0
+length = len(photos)
 while i < length:
     shutil.copy2(f"C:\\Users\\{home_directory}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\{photos[i]}",f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\{foldername}\\{photos[i]}.jpg")
     i += 1
 
-""" 
-os.chdir(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\{foldername}")
+#creates a folder for landscape photos
+if not os.path.exists(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\_Landscape_Photos"):    
+    os.mkdir(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\_Landscape_Photos")
+if not os.path.exists(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\_Landscape_Photos\\_Landscape_{foldername}"):    
+    os.mkdir(f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\_Landscape_Photos\\_Landscape_{foldername}")
+
+#copy only landscape photos to _Landscape_Photos
 i = 0
 length = len(photos)
 while i < length:
-    newphoto = photos[i]
-    image = Image.open(f"{newphoto}.jpg")
-    exifdata = image.getexif()
-    for tag_id in exifdata:
-    # get the tag name, instead of human unreadable tag id
-        tag = TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id)
-    # decode bytes 
-        if isinstance(data, bytes):
-            data = data.decode()
-        print(f"{tag:25}: {data}")
+    cv_filepath = f"C:\\Users\\{home_directory}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\{photos[i]}"
+    image = cv2.imread(cv_filepath)
+    height, width = image.shape[:2]
+    if width == 1920:
+        shutil.copy2(f"C:\\Users\\{home_directory}\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\{photos[i]}",f"C:\\Users\\{home_directory}\\OneDrive\\Desktop\\Spotlight\\_Landscape_Photos\\_Landscape_{foldername}\\{photos[i]}.jpg")
     i += 1
- """
-
